f9ad583c1f1d57daae8dbaa221a5d4d9974d1d0d
KamiZomby <65251450+KamiZomby@users.noreply.github.com>
2020-06-28 14:27:55 +0100

    Images added + index.html

---
+++ src/components/products.js
@@ -0,0 +1,37 @@
+// Product 1
+var app = new Vue({
+    el: '#product1',
+    data: {
+        id: 1,
+        title: "Flower 1",
+        price: 10,
+        inStock: true,
+        image: '/src/images/Bouquet01.webp'
+    }
+})
+
+
+// Product 2
+var app = new Vue({
+    el: '#product2',
+    data: {
+        id: 2,
+        title: "Flower 2",
+        price: 10,
+        inStock: true,
+        image: '/src/images/Bouquet2.webp'
+    }
+})
+
+
+// Product 3
+var app = new Vue({
+    el: '#product3',
+    data: {
+        id: 3,
+        title: "Flower 3",
+        price: 10,
+        inStock: true,
+        image: '/src/images/Bouquet3.webp'
+    }
+})
