319cf24674f9472d38c805d8ee481eb0b37376ef
KamiZomby <65251450+KamiZomby@users.noreply.github.com>
2020-06-28 13:44:01 +0100

    More components

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+
+const logo = '/src/images/Flower-Logo.png';
+
+document.getElementById("header").innerHTML = `
+    <div>
+        <img src="${logo}" alt="logo">
+        <div>
+            <p>idfbn</p>
+        </div>
+    </div>
+`;
